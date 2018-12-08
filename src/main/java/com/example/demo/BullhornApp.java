e8c37d644aa7fff5d951e61189e3d7da06e49e66
tarnejahrolfe <tarnejahrolfe@gmail.com>
2018-12-07 20:57:11 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/BullhornApp.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BullhornApp {
+    public static void main(String[] args) {
+        SpringBootApplication.run(BullhornApp.class, args);
+    }
+}
